2d220303b089ede377ad03e516f4d9cf1aa8098a
Vinay Jeurkar <vjeurkar03@gmail.com>
2023-08-31 21:30:36 +0530

    Updated output variables

--- aws-golang-lambda-cloudwatch/output.tf
+++ aws-golang-lambda-cloudwatch/output.tf
@@ -1,7 +1,3 @@
-output "lambda_environment" {
-	value = aws_lambda_function.test_lambda.environment
-}
-
 output "lambda_function_url" {
 	value = aws_lambda_function_url.lambda_function_url.function_url
 	description = "This URL is used to call the lambda function"
