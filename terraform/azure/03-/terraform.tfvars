5a1aef68a1897f11eceeb3bd9d17bbb466b14643
Vinay Jeurkar <vjeurkar03@gmail.com>
2023-10-25 16:46:59 +0530

    Added Variables

---
+++ terraform/azure/03-/terraform.tfvars
@@ -0,0 +1,29 @@
+resource_group_name = "my-app"
+location_id = "southindia"
+
+virtual_network_name = "myapp-vnet"
+virtual_network_cidr = "10.0.0.0/16"
+
+websubnet_name = "web-subnet"
+websubnet_cidr = "10.0.1.0/24"
+web_vm_size = "Standard_B1s"
+web_hostname = "webserver"
+web_username = "webuser"
+web_password = "p@ssw0rd"
+
+dbsubnet_name = "db-subnet"
+dbsubnet_cidr = "10.0.2.0/24"
+db_server_name = "sql-database"
+db_name = "sql-database"
+db_version = "8.0"
+db_username = "sqladmin"
+db_password = "p@ssw0rd"
+
+appsubnet_name = "app-subnet"
+appsubnet_cidr = "10.0.2.0/24"
+app_vm_size = "Standard_B1s"
+app_hostname = "appserver"
+app_username = "appuser"
+app_password = "p@ssw0rd"
+
+securitygroup_name = "sg-group"
---
+++ terraform/azure/03-/variable.tf
@@ -0,0 +1,30 @@
+variable "resource_group_name" { }
+variable "location_id" { }
+
+variable "virtual_network_name" { }
+variable "virtual_network_cidr" { }
+
+variable "websubnet_name" { }
+variable "websubnet_cidr" { }
+variable "web_vm_size" { }
+variable "web_hostname" { }
+variable "web_username" { }
+variable "web_password" { }
+
+variable "dbsubnet_name" { }
+variable "dbsubnet_cidr" { }
+variable "db_server_name" { }
+variable "db_name" { }
+variable "db_version" { }
+variable "db_username" { }
+variable "db_password" { }
+
+
+variable "appsubnet_name" { }
+variable "appsubnet_cidr" { }
+variable "app_vm_size" { }
+variable "app_hostname" { }
+variable "app_username" { }
+variable "app_password" { }
+
+variable "securitygroup_name" { }
